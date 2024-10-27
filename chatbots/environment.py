d830ce416f26e6e3b7b5dfb9f85c41e5c176b28a
cintiaching <cintiaching@lcjgroup.com>
2024-10-27 14:16:46 +0800

    read data

---
+++ chatbots/environment.py
@@ -0,0 +1,19 @@
+from dotenv import load_dotenv
+import os
+import polars as pl
+
+load_dotenv()  # Load environment variables from .env file
+
+# programmatically detect if running on databricks
+IS_DATABRICKS: bool = "DATABRICKS_RUNTIME_VERSION" in os.environ
+
+DATA_DIRECTORY = os.environ.get("DATA_DIRECTORY")
+
+
+def read_dateset() -> pl.DataFrame:
+    if IS_DATABRICKS:
+        spark_df = spark.table("bright_data_best_buy_products_dataset.datasets.best_buy_products").toPandas()
+        df = pl.from_pandas(spark_df)
+    else:
+        df = pl.read_csv(os.path.join(DATA_DIRECTORY, "best_buy_products.csv"), ignore_errors=True)
+    return df
